e19c21f2b1556012853b3b93da758cc1d36303f1
Pranjali <damdharpranjali1@gmail.com>
2026-08-27 22:23:33 +0530

    Update addOne.java

---
+++ LinkedList/add one/addOne.java
@@ -0,0 +1,36 @@
+/* Structure of linked list Node
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public int helper(Node temp){
+        if(temp == null){
+            return 1;
+        }
+        int carry = helper(temp.next);
+        temp.data = temp.data+carry;
+        if(temp.data<10){
+            return 0;
+        }
+        temp.data = 0;
+        return 1;
+        
+    }
+    public Node addOne(Node head) {
+        int carry = helper(head);
+        if(carry == 1){
+            Node NewNode = new Node(1);
+            NewNode.next = head;
+            return NewNode;
+        }
+        return head;
+        
+    }
+}
