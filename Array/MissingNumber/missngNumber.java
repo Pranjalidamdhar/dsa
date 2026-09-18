786a0af9f7a61158c946af408405e35dadd131ef
Pranjali <damdharpranjali1@gmail.com>
2026-09-18 21:51:12 +0530

    Add files via upload

---
+++ Array/MissingNumber/missngNumber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+
+       int XOR1 = 0;
+       int XOR2 = 0;
+       for(int i=0;i<nums.length;i++){
+        XOR2 ^= nums[i];
+        XOR1 ^= (i+1);
+       } 
+       return XOR1^XOR2;
+    }
+}
